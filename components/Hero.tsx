97a2f317731eca7b135cbe73ad5dc7ffc0de6560
Johan George <johangeorge201203@gmail.com>
2025-08-30 13:52:37 +0530

    responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,7 +34,7 @@ export function Hero() {
   return (
     <div className="relative w-full h-[100svh] overflow-hidden bg-jwhite ">
       {/* Blurred Circle Background */}
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] rounded-full bg-joranage z-20"></div>
+  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[350px] h-[350px] md:w-[500px] md:h-[500px] lg:w-[600px] lg:h-[600px] rounded-full bg-joranage z-20"></div>
       {/* Prism Background only for hero section */}
    
       <Navbar className="absolute top-0 left-0 w-full z-10 pt-2">
@@ -95,7 +95,7 @@ export function Hero() {
  
         {/* Hero Content */}
   <div className="flex flex-col items-center justify-center h-full px-4 text-center z-30 relative">
-          <h1 className="text-4xl md:text-9xl  font-jfont text-jblue font-bold mb-4">FOSS FEST 25</h1>
+          <h1 className="text-6xl md:text-9xl  font-jfont text-jblue font-bold mb-4">FOSS FEST 25</h1>
           <p className="text-lg md:text-2xl text-white mb-6">Unlocking the Code: A Festival of Freedom</p>
           <p className="text-lg md:text-2xl text-white mb-6">27th September 2025 . Krunagappally</p>
 

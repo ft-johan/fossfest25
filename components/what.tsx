8100983ba8a3d4b4fd7d3dfce522d0ca8ac809ac
Johan George <johangeorge201203@gmail.com>
2025-09-23 23:10:45 +0530

    refactor: restructure What component for improved layout and readability

--- components/what.tsx
+++ components/what.tsx
@@ -7,7 +7,8 @@ export default function What() {
     return (
         <section className="w-full h-[50svh] flex flex-col items-center justify-center py-20 px-4">
             
-             <ScrollFloat
+           <div className='md:block hidden'>
+           <ScrollFloat
   animationDuration={1}
   ease='back.inOut(2)'
   scrollStart='bottom bottom+=20%'
@@ -16,7 +17,10 @@ export default function What() {
 
 >
 what is Foss fest?
-</ScrollFloat>
+</ScrollFloat></div>
+              <div className='md:hidden block '>
+            <p className='text-jblue font-jfont font-bold text-center mb-6 text-4xl md:text-5xl'>What is <span className='text-joranage'>Foss fest?</span></p>
+            </div>
             <p className="max-w-xl text-center text-lg md:text-xl text-black mx-auto">
 Whether you &apos;re a seasoned developer, a curious student, a UI/UX designer, or simply someone passionate about the power of open-source technology, FOSS Fest &apos;25 is for you.
 
